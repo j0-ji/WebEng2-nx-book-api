9541b3ea07c10b5d6d6c2b49cb8ceaa448ad3370
Jōji <gpgerov+github@gmail.com>
2024-05-17 12:48:35 +0200

    added README + updated books

--- apps/backend-express/src/books/controllers/books.contorller.ts
+++ apps/backend-express/src/books/controllers/books.contorller.ts
@@ -1,9 +1,9 @@
 'use strict';
 
-import express, {type Request, type Response} from 'express';
+import express, { type Request, type Response } from 'express';
 import { type Book, type CreateBookDto, type UpdateBookDto } from '@nx-book-api/contract';
-import { checkIfUpdateBookDto, checkIfCreateBookDto } from "../utils/books.util";
-import { BookService } from "../services/books.service";
+import { checkIfUpdateBookDto, checkIfCreateBookDto } from '../utils/books.util';
+import { BookService } from '../services/books.service';
 import { ObjectId } from 'mongodb';
 
 // ROUTER
@@ -14,109 +14,108 @@ const bookService = new BookService();
 
 // HTTP-Requests
 router.get('/', async (req: Request, res: Response) => {
-    try {
-        const serviceResponse = await bookService.getBooks()
-        const books: Book[] = serviceResponse.result as Book[];
-        return res.status(200).json(books);
-    } catch (error) {
-        console.error('ERROR while fetching books: ', error);
-        return res.status(500).send('ERROR while fetching books!');
-    }
+  try {
+    const serviceResponse = await bookService.getBooks();
+    const books: Book[] = serviceResponse as Book[];
+    return res.status(200).json(books);
+  } catch (error) {
+    return res.status(500).send({ message: 'DB down' });
+  }
 });
 
 router.get('/:id', async (req: Request, res: Response) => {
-    const bookId = req?.params?.id;
+  const bookId = req?.params?.id;
 
-    try {
-        const serviceResponse = await bookService.getBookById(new ObjectId(bookId));
-        if ( serviceResponse.result ) {
-            return res.status(200).json(serviceResponse);
-        } else {
-            return res.status(404).send('Book not found!');
-        }
-    } catch (error) {
-        res.status(500).send('Error fetching book!');
+  try {
+    const serviceResponse = await bookService.getBookById(new ObjectId(bookId));
+    if (serviceResponse) {
+      return res.status(200).json(serviceResponse);
+    } else {
+      return res.status(404).send('Book not found!');
     }
+  } catch (error) {
+    res.status(500).send('Error fetching book!');
+  }
 });
 
 router.post('/', async (req: Request, res: Response) => {
-    if (checkIfCreateBookDto(req?.body)) {
-        const bookDto = req.body as CreateBookDto;
+  if (checkIfCreateBookDto(req?.body)) {
+    const bookDto = req.body as CreateBookDto;
 
-        try {
-            const serviceResponse = await bookService.createBook(bookDto);
-            if (serviceResponse.result.acknowledged) {
-                const _id = serviceResponse.result.insertedId.toString();
-                return res.status(200).json({_id, ...bookDto});
-            } else {
-                return res.status(500).send('failed adding book');
-            }
-        } catch (error) {
-            return res.status(500).send('failed adding book');
-        }
-    } else {
-        return res.status(400).send('Invalid book attributes');
+    try {
+      const serviceResponse = await bookService.createBook(bookDto);
+      if (serviceResponse.acknowledged) {
+        const _id = serviceResponse.insertedId.toString();
+        return res.status(200).json({ _id, ...bookDto });
+      } else {
+        return res.status(500).send('failed adding book');
+      }
+    } catch (error) {
+      return res.status(500).send('failed adding book');
     }
+  } else {
+    return res.status(400).send('Invalid book attributes');
+  }
 });
 
 router.patch('/:id', async (req: Request, res: Response) => {
-    const bookId = req?.params?.id;
+  const bookId = req?.params?.id;
 
-    if (checkIfUpdateBookDto(req?.body)) {
-        const bookDto = req.body as UpdateBookDto;
-
-        try {
-            const serviceResponse = await bookService.updateBook(new ObjectId(bookId), bookDto);
-
-            if (serviceResponse.result.modifiedCount === 1) {
-                return res.status(200).json(serviceResponse.newBook);
-            } else {
-                return res.status(400).send('book already has the passed attributes');
-            }
-        } catch (error) {
-            return res.status(404).send('Book with given ID not found');
-        }
-    } else {
-        return res.status(400).send('invalid book attributes');
-    }
-});
-
-router.put('/:id', async (req: Request, res: Response)=> {
-    const bookId = req?.params?.id;
-
-    if (checkIfCreateBookDto(req?.body)) {
-        const bookDto = req.body as CreateBookDto;
-
-        try {
-            const serviceResponse = await bookService.replaceBook(new ObjectId(bookId), bookDto);
-
-            if (serviceResponse.result.modifiedCount === 1) {
-                return res.status(200).json(serviceResponse.newBook);
-            } else {
-                return res.status(404).send('book not found');
-            }
-        } catch (error) {
-            return res.status(404).send('Book with given ID not found');
-        }
-    } else {
-        return res.status(400).send('invalid book attributes');
-    }
-})
-
-router.delete('/:id', async (req: Request, res: Response) => {
-    const bookId = req?.params?.id;
+  if (checkIfUpdateBookDto(req?.body)) {
+    const bookDto = req.body as UpdateBookDto;
 
     try {
-        const serviceResponse = await bookService.deleteBook(new ObjectId(bookId));
+      const serviceResponse = await bookService.updateBook(new ObjectId(bookId), bookDto);
 
-        if (serviceResponse.result.deletedCount === 1) {
-            return res.status(200).send('book deleted successfully');
-        } else {
-            return res.status(404).send('book not found');
-        }
+      if (serviceResponse.result.modifiedCount === 1) {
+        return res.status(200).json(serviceResponse.newBook);
+      } else {
+        return res.status(400).send('book already has the passed attributes');
+      }
     } catch (error) {
-        res.status(500).send('error deleting book');
+      return res.status(404).send('Book with given ID not found');
     }
+  } else {
+    return res.status(400).send('invalid book attributes');
+  }
+});
+
+router.put('/:id', async (req: Request, res: Response) => {
+  const bookId = req?.params?.id;
+
+  if (checkIfCreateBookDto(req?.body)) {
+    const bookDto = req.body as CreateBookDto;
+
+    try {
+      const serviceResponse = await bookService.replaceBook(new ObjectId(bookId), bookDto);
+
+      if (serviceResponse.result.modifiedCount === 1) {
+        return res.status(200).json(serviceResponse.newBook);
+      } else {
+        return res.status(404).send('book not found');
+      }
+    } catch (error) {
+      return res.status(404).send('Book with given ID not found');
+    }
+  } else {
+    return res.status(400).send('invalid book attributes');
+  }
+});
+
+router.delete('/:id', async (req: Request, res: Response) => {
+  const bookId = req?.params?.id;
+
+  try {
+    const serviceResponse = await bookService.deleteBook(new ObjectId(bookId));
+
+    if (serviceResponse.deletedCount === 1) {
+      return res.status(200).send('book deleted successfully');
+    } else {
+      return res.status(404).send('book not found');
+    }
+  } catch (error) {
+    res.status(500).send('error deleting book');
+  }
 });
 
 export { router };
--- apps/backend-express/src/books/services/books.service.ts
+++ apps/backend-express/src/books/services/books.service.ts
@@ -11,62 +11,52 @@ const collection = client.db(databaseName).collection(collectionName);
 // TODO: divide Service better from the module
 
 export class BookService {
-    // get all books
-    public async getBooks() {
-        const result = await collection.find({}).toArray() as unknown as Book[];
-        return {
-            result: result,
-        }
-    }
+  // get all books
+  public async getBooks() {
+    const result = await collection.find({}).toArray() as unknown as Book[];
+    return result;
+  }
 
-    public async getBookById(id: Book['_id']) {
-        const query = {_id: id};
-        const result = await collection.findOne(query) as unknown as Book;
-        return {
-            result: result,
-        }
-    }
+  public async getBookById(id: Book['_id']) {
+    const query = { _id: id };
+    return await collection.findOne(query) as unknown as Book;
+  }
 
-    public async createBook(dto: CreateBookDto) {
-        const result = await collection.insertOne(dto);
-        return {
-            result: result,
-        }
-    }
+  public async createBook(dto: CreateBookDto) {
+    return await collection.insertOne(dto);
+  }
 
-    public async updateBook(id: Book['_id'], dto: UpdateBookDto) {
-        const query = { _id: id};
-        const result = await collection.updateOne(
-            query,
-            { $set: dto }
-        );
-        const newBook: Book = await collection.findOne(query) as Book;
+  public async updateBook(id: Book['_id'], dto: UpdateBookDto) {
+    const query = { _id: id };
+    const result = await collection.updateOne(
+      query,
+      { $set: dto },
+    );
+    const newBook: Book = await collection.findOne(query) as Book;
 
-        return {
-            result: result,
-            newBook: newBook
-        };
-    }
+    return {
+      result: result,
+      newBook: newBook,
+    };
+  }
 
-    public async replaceBook(id: Book['_id'], dto: CreateBookDto) {
-        const query = { _id: id};
-        const result =  await collection.updateOne(
-            query,
-            { $set: dto }
-        );
-        const newBook: Book = {_id: id, ...dto};
+  public async replaceBook(id: Book['_id'], dto: CreateBookDto) {
+    const query = { _id: id };
+    const result = await collection.updateOne(
+      query,
+      { $set: dto },
+    );
+    const newBook: Book = { _id: id, ...dto };
 
-        return {
-            result: result,
-            newBook: newBook
-        };
-    }
+    return {
+      result: result,
+      newBook: newBook,
+    };
+  }
 
-    public async deleteBook(id: Book['_id']) {
-        const query = { _id: id};
-        const result = await collection.deleteOne(query);
-        return {
-            result: result,
-        };
-    }
+  public async deleteBook(id: Book['_id']) {
+    const query = { _id: id };
+    const result = await collection.deleteOne(query);
+    return result
+  }
 }
